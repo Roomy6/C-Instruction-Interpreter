bf6002d3a58b8a61f92dff4b7517a813bf4f6179
Roomy6 <roomyk792@gmail.com>
2026-06-27 17:19:23 +0200

    Better Hex/Ascii representation of program

--- main.c
+++ main.c
@@ -10,13 +10,11 @@
  * 27/06/2026 - 15:32
  */
 
-#include <stdio.h>
 #include <ctype.h>
 #include <string.h>
 #include <stdbool.h>
 
-#define MAX_INSTRUCTIONS 1024
-#define MAX_TOKEN_LEN 64
+#include "helper.h"
 
 typedef struct
 {
@@ -26,6 +24,82 @@ typedef struct
 /* Array to simulate the "compiled" program */
 Instruction program[MAX_INSTRUCTIONS];
 int instruction_count = 0;
+
+void readFile(FILE* file)
+{
+    unsigned char buffer[HEX_DISPLAY];
+    size_t bytesRead, prevBytesRead = 0;
+    long address = 0;
+
+    int ch;
+    bool in_comment = false;
+    char token[MAX_TOKEN_LEN];
+    int token_idx = 0;
+
+    while((bytesRead = fread(buffer, 1, sizeof(buffer), file)) >0 )
+    {
+        printLine(buffer, bytesRead, address);
+
+        for(size_t i = 0; i < bytesRead; i++)
+        {
+            ch = buffer[i];
+
+            if(ch == '[')
+            {
+                in_comment = true;
+                continue;
+            }
+            if(ch == ']')
+            {
+                in_comment = false;
+                continue;
+            }
+
+            if(in_comment)
+                continue;
+
+            if(isspace(ch))
+            {
+                if(token_idx > 0)
+                {
+                    /* NULL terminate */
+                    token[token_idx] = '\0';
+
+                    /* Instruction validation */
+                    if(strcmp(token, "HELLO") == 0 || strcmp(token, "BYE") == 0)
+                    {
+                        strcpy(program[instruction_count].name, token);
+                        instruction_count++;
+                    } else
+                    {
+                        printf("\nCOMPILE ERROR: Unknown instruction '%s'\n", token);
+                        return;
+                    }
+                    /* Reset for the next word */
+                    token_idx = 0;
+                }
+            } else
+            {
+                if(token_idx < MAX_TOKEN_LEN - 1)
+                    token[token_idx++] = ch;
+            }
+        }
+        address += bytesRead;
+    }
+
+    /* Check for instruction at EOF */
+    if(token_idx > 0)
+    {
+        token[token_idx] = '\0';
+        if(strcmp(token, "HELLO") == 0 || strcmp(token, "BYE") == 0)
+        {
+            strcpy(program[instruction_count++].name, token);
+        } else
+        {
+            printf("\nCOMPILE ERROR: Unknown instruction '%s'\n", token);
+        }
+    }
+}
 
 int main(int argc, char* argv[])
 {
@@ -39,82 +113,15 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    int ch;
-    bool in_comment = false;
-    char token[MAX_TOKEN_LEN];
-    int token_idx = 0;
-
     printf("--- COMPILING ---h\n");
 
-    while((ch = fgetc(file)) != EOF)
-    {
-        if(!isspace(ch))
-            printf("Char: '%c' | HEX: %02X\n", ch, (unsigned char)ch);
-
-        if(ch == '[')
-        {
-            in_comment = true;
-            continue;
-        }
-        if(ch == ']')
-        {
-            in_comment = false;
-            continue;
-        }
-
-        if(in_comment)
-            continue;
-
-        if(isspace(ch))
-        {
-            if(token_idx > 0)
-            {
-                /* NULL terminate */
-                token[token_idx] = '\0';
-
-                /* Instruction validation */
-                if(strcmp(token, "HELLO") == 0 || strcmp(token, "BYE") == 0)
-                {
-                    strcpy(program[instruction_count].name, token);
-                    instruction_count++;
-                } else
-                {
-                    printf("\nCOMPILE ERROR: Unknown instruction '%s'\n", token);
-                    fclose(file);
-                    return 1;
-                }
-                /* Reset for the next word */
-                token_idx = 0;
-            }
-        } else
-        {
-            if(token_idx < MAX_TOKEN_LEN - 1)
-                token[token_idx++] = ch;
-        }
-        
-    }
-
-    /* Check for instruction at EOF */
-    if(token_idx > 0)
-    {
-        token[token_idx] = '\0';
-        if(strcmp(token, "HELLO") == 0 || strcmp(token, "BYE") == 0)
-        {
-            strcpy(program[instruction_count++].name, token);
-        } else
-        {
-            printf("\nCOMPILE ERROR: Unknown instruction '%s'\n", token);
-            fclose(file);
-            return 1;
-        }
-    }
+    readFile(file);
 
     fclose(file);
-    printf("COMPILATION SUCCESSFULL! %d instruction loaded.\n\n", instruction_count);
+    printf("\nCOMPILATION SUCCESSFULL! %d instruction loaded.\n\n", instruction_count);
 
     printf("--- EXECUTING ---\n");
 
-    /* Run the instructions */
     for(int i = 0; i < instruction_count; i++)
     {
         if(strcmp(program[i].name, "HELLO") == 0)
